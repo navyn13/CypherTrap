00db58784af02f965af4b8b8fde207b5db11ee1d
Naveen <navyn13102003@gmail.com>
2026-06-13 23:55:13 +0530

    remove normal map so to use redis

--- cmd/server/algorithms.go
+++ cmd/server/algorithms.go
@@ -5,20 +5,13 @@ type Algorithm interface {
 }
 type fixedWindowAlgorithm struct {
 	windowSize int
-	window     map[string]int
 }
 
 func NewFixedWindowAlgorithm(windowSize int) Algorithm {
 	return &fixedWindowAlgorithm{
 		windowSize: windowSize,
-		window:     make(map[string]int),
 	}
 }
 func (a *fixedWindowAlgorithm) Check(ip string) bool {
-	if a.window[ip] >= a.windowSize {
-		return false
-	}
-
-	a.window[ip]++
 	return true
 }
